a5681668d209ff32128e8a37da4dd9bbac72aa75
Boris <boris.grinshpun@hotmail.com>
2023-01-30 14:21:00 +0200

    sync issues fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,11 +19,10 @@ function App() {
   const [userId, setUserId] = useState("")
   const [connected, setConnected] = useState(false)
   const [song, setSong] = useState(null)
-  const [playlistLoaded, setPlaylistLoaded] = useState(false)
+  // const [playlistLoaded, setPlaylistLoaded] = useState(false)
 
   // fetch initial playlist from cdn, could be empty
   useEffect(() => {
-    playlistLoaded
     try {
       (async () => {
         const cdnPlaylist = await fetchPlaylistFromCDN()
@@ -31,55 +30,36 @@ function App() {
         if (cdnPlaylist.length) {
           const updatedList = await updatePlaylistTitlesFromYoutube(cdnPlaylist)
           setPlaylist([...updatedList])
-          setPlaylistLoaded(true)
         }
+        setupSEE()
       })()
     } catch (err) {
       console.log(err)
     }
   }, [])
 
-  // subscribe to server side events to get playlist updates
+  // handle playing song edge cases on plalist change
   useEffect(() => {
-    if (!connected && playlistLoaded) {
-      try {
-        const clientTimestamp = Date.now()
-        const evtSource = new EventSource(`${SUBSCTIPTION_URL}/${clientTimestamp}`, {
-          withCredentials: false,
-        });
-        evtSource.addEventListener(`${USER_REGISTRATION_EVENT}`, (event) => {
-          const data = JSON.parse(event.data)
-          setUserId(data.userId)
-        })
-        evtSource.addEventListener(PLAYLIST_DIFF_EVENT, (event) => {
-          const response = JSON.parse(event.data)
-          if (response.data.length) {
-            setDiff(response.data)
-          }
-        })
-      } catch (err) {
-        console.log(err)
-      }
-      setConnected(true)
+    // set the first song
+    if (playlist.length == 1 && song === null) {
+      setSong({ ...playlist[0] })
     }
-  }, [connected, playlistLoaded])
 
-  // update playlist when songs are added by other clients
-  useEffect(() => {
-    if (diff) {
-      const uniqueSongs = diff.filter(song => {
-        const res = playlist.find(item => item.ifd === song.id)
-        if (res === undefined) return true
-        return false
-      })
-      if (uniqueSongs && uniqueSongs.length) {
-        (async () => {
-          const updatedSongs = await updatePlaylistTitlesFromYoutube(uniqueSongs)
-          setPlaylist([...playlist, ...updatedSongs])
-        })()
-      }
+    // empty playlist
+    if (!playlist.length)
+      setSong(null)
+
+    // handle playing song removal
+    if (playlist.length && song && song.id !== playlist[0].id)
+      setSong({ ...playlist[0] })
+
+  }, [playlist])
+
+  useEffect(()=>{
+    if (diff.length) {
+      mergeDiffToPlaylist()
     }
-  }, [diff])
+  },[diff])
 
   // 1. update playlist when new song is added
   // 2. send a request to update the playlist on cdn
@@ -102,8 +82,8 @@ function App() {
   }
   // play next song handler
   const onSongEndHandler = useCallback(() => {
-    if (playlist.length > 1){
-      setSong({...playlist[1]})
+    if (playlist.length > 1) {
+      setSong({ ...playlist[1] })
     } else {
       setSong(null)
     }
@@ -115,41 +95,68 @@ function App() {
   const onSongErrorHandler = useCallback(() => {
     onSongEndHandler()
   })
-  
+
   const removeSongHandler = (index) => {
     const newPlaylist = [...playlist]
     newPlaylist.splice(index, 1)
     setPlaylist(newPlaylist)
   }
 
-  useEffect(()=>{
-    // set the first song
-    if (playlist.length == 1 && song === null){
-      setSong({...playlist[0]})
+  // subscribe to server side events to get playlist updates
+  function setupSEE() {
+    if (!connected) {
+      console.log('connecting...')
+      try {
+        const clientTimestamp = Date.now()
+        const evtSource = new EventSource(`${SUBSCTIPTION_URL}/${clientTimestamp}`, {
+          withCredentials: false,
+        });
+        evtSource.addEventListener(`${USER_REGISTRATION_EVENT}`, (event) => {
+          const data = JSON.parse(event.data)
+          setUserId(data.userId)
+        })
+        evtSource.addEventListener(PLAYLIST_DIFF_EVENT, (event) => {
+          const response = JSON.parse(event.data)
+          if (response.data.length) {
+            setDiff(response.data)
+          }
+        })
+      } catch (err) {
+        console.log(err)
+      }
+      console.log('connected')
+      setConnected(true)
     }
+  }
 
-    // empty playlist
-    if (!playlist.length)
-      setSong(null)
-    
-    // handle playing song removal
-    if (playlist.length && song && song.id !== playlist[0].id)
-      setSong({...playlist[0]})
-  },[playlist])
-
-  // const songId = playlist && playlist.length ? playlist[0].songId : null
-  // const id = playlist && playlist.length ? playlist[0].id : null
+  // update playlist when songs are added by other clients
+  function mergeDiffToPlaylist() {
+    if (diff) {
+      const uniqueSongs = diff.filter(song => {
+        const res = playlist.find(item => item.id === song.id)
+        if (res === undefined) return true
+        return false
+      })
+      if (uniqueSongs && uniqueSongs.length) {
+        (async () => {
+          const updatedSongs = await updatePlaylistTitlesFromYoutube(uniqueSongs)
+          setPlaylist([...playlist, ...updatedSongs])
+          setDiff([])
+        })()
+      }
+    }
+  }
 
   return (
     <div className="App">
       <div className="container">
         <aside>
-          <Search className="search" onAddSong={addSongHandler}/>
-          <Playlist list={playlist} onSongRemove={removeSongHandler}/>
+          <Search className="search" onAddSong={addSongHandler} />
+          <Playlist list={playlist} onSongRemove={removeSongHandler} />
         </aside>
         <main>
         </main>
-        <Player song={song} onSongEnd={onSongEndHandler} onSongError={onSongErrorHandler}/>
+        <Player song={song} onSongEnd={onSongEndHandler} onSongError={onSongErrorHandler} />
       </div>
     </div>
   )
