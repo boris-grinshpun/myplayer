d5998ceb6ee44172808a9547df4de385b9c93539
Boris <rockarena@gmail.com>
2023-01-30 14:24:01 +0200

    localhost in url might break server

--- server/common/constants.js
+++ server/common/constants.js
@@ -1,3 +1,3 @@
-export const CDN_DEV_PLAYLIST_URL = "http://localhost:8080/playlist.json"
+export const CDN_DEV_PLAYLIST_URL = "http://127.0.0.1:8080/playlist.json"
 
 export const UPDATE_INTERVAL = 5000
